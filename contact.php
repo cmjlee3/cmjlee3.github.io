252fce2052bdd2874ce6a304e6e86b9f102dc344
Cecil Lee <cmjlee3@gmail.com>
2017-01-30 22:41:58 -0500

    parallax pic

---
+++ contact.php
@@ -0,0 +1,35 @@
+<?php
+if(isset($_POST['submit'])){
+    $to = "cmjlee3@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title></title>
+  <link rel="stylesheet" href="">
+</head>
+<body>
+  echo $message;
+  <h1>Thank You</h1>
+  <<p>Your message has been sent.</p>
+  <h1>Oops!</h1>
+  <<p>Sorry, there was a problem sending your message</p>
+</body>
+</html>
